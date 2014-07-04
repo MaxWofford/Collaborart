aad616ff4be02a0ea2fe9bc69afeeced8cb9f7f4
MaxWofford <max.speed.wofford@gmail.com>
2014-07-04 14:29:34 -0700

    Various changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'layouts/_footer'
+
+  get 'layouts/header'
+
+  get 'search' => 'pages#search'
+  get 'contact' => 'pages#contact'
+  root 'pages#home'  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
