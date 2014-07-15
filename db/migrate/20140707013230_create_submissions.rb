a43df023345347c5b629f89dbb818994cddba9f0
MaxWofford <max.speed.wofford@gmail.com>
2014-07-15 02:47:40 -0700

    Added title to submissions migration

--- db/migrate/20140707013230_create_submissions.rb
+++ db/migrate/20140707013230_create_submissions.rb
@@ -2,7 +2,7 @@ class CreateSubmissions < ActiveRecord::Migration
   def change
     create_table :submissions do |t|
       t.text :description
-
+      t.string :title
       t.timestamps
     end
   end
