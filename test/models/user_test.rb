47fb08405c75c8cebd64a3033ffe73663e14d617
MaxWofford <max.speed.wofford@gmail.com>
2014-07-19 20:35:42 -0700

    Added user generation testcase

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "User creation" do
+
+    puts "Creating User for test"
+    user = User.new
+
+    puts "Testing if test User is valid"
+    assert user.invalid?
+    assert user.errors[:email].any?
+    assert user.errors[:password].any?
+  end
 end
