bc2cd7f6e073058a5a48c5a28028eaa6ecd5527f
MaxWofford <max.speed.wofford@gmail.com>
2014-07-20 01:24:15 -0700

    Fixed user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,12 +6,13 @@ class UserTest < ActiveSupport::TestCase
   # end
   test "User creation" do
 
-    puts "Creating User for test"
-    user = User.new
+    puts "Creating user for test"
+    user = User.new(:email => "testuser@example.com", :password => "12345678", :username => "TestUser")
 
-    puts "Testing if test User is valid"
-    assert user.invalid?
-    assert user.errors[:email].any?
-    assert user.errors[:password].any?
+    puts "Testing if user is valid"
+    assert(user.valid?, "User is not valid")
+    assert(user.errors[:email].none?, "Email is not valid")
+    assert(user.errors[:password].none?, "Password is not valid")
+    assert(user.errors[:username].none?, "Username is not valid")
   end
 end
